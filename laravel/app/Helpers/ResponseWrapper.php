91ec78502deed88c8a029a065cacb65d9367cfae
Audric Andhika <audricc015@gmail.com>
2025-12-05 23:25:10 +0700

    feat: add ResponseWrapper helper for API responses

---
+++ laravel/app/Helpers/ResponseWrapper.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Helpers;
+
+class ResponseWrapper
+{
+    public static function make(
+        string $message,
+        int $status,
+        bool $success,
+        array|null $data,
+        array|null $errors,
+    ) {
+        if (!is_null($errors)) {
+            return response()->json(
+                [
+                    "message" => $message,
+                    "data" => $data,
+                    "error" => $errors,
+                    "success" => $success,
+                ],
+                $status,
+            );
+        }
+        return response()->json(
+            [
+                "message" => $message,
+                "data" => $data,
+                "error" => $errors,
+                "success" => $success,
+            ],
+            $status,
+        );
+    }
+}
