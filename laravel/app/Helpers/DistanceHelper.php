d6c39175e57e77438bf2e805ccef0484be343608
Audric Andhika <audricc015@gmail.com>
2025-12-05 23:24:28 +0700

    feat: add DistanceHelper

---
+++ laravel/app/Helpers/DistanceHelper.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!function_exists('haversine_distance')) {
+    function haversine_distance($lat1, $lon1, $lat2, $lon2)
+    {
+        $earthRadius = 6371000; // meter
+
+        $dLat = deg2rad($lat2 - $lat1);
+        $dLon = deg2rad($lon2 - $lon1);
+
+        $a = sin($dLat/2) * sin($dLat/2) +
+             cos(deg2rad($lat1)) * cos(deg2rad($lat2)) *
+             sin($dLon/2) * sin($dLon/2);
+
+        $c = 2 * atan2(sqrt($a), sqrt(1-$a));
+        return $earthRadius * $c; // jarak dalam meter
+    }
+}
